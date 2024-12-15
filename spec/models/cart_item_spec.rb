6b574ae1c2beb65b12e84fb59d6e5a38057df1b7
Marina Alane <marina.alane@outlook.com>
2024-12-15 16:53:17 -0300

    chore: add tests for cartItem model

--- spec/models/cart_item_spec.rb
+++ spec/models/cart_item_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe CartItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:cart) { create(:cart) }
+  let(:product) { create(:product, price: 10.00) }
+  let(:cart_item) { create(:cart_item, cart: cart, product: product, quantity: 2) }
+
+  it 'belongs to a cart' do
+    expect(cart_item.cart).to eq(cart)
+  end
+
+  it 'belongs to a product' do
+    expect(cart_item.product).to eq(product)
+  end
+
+  describe '#update_cart_total_price' do
+    it 'updates the cart total price after create' do
+      expect {
+        create(:cart_item, cart: cart, product: product, quantity: 3)
+      }.to change { cart.reload.total_price }.by(30.0)
+    end
+
+    it 'updates the cart total price after update' do
+      cart.update_total_price
+
+      expect {
+        cart_item.update(quantity: 5)
+      }.to change { cart.reload.total_price }.by(50.0)
+    end
+  end
 end
--- spec/requests/carts_spec.rb
+++ spec/requests/carts_spec.rb
@@ -5,15 +5,14 @@ RSpec.describe "/carts", type: :request do
   describe "POST /add_items" do
     let(:cart) { create(:cart) }
     let(:product) { create(:product) }
-    let!(:cart_item) { create(:cart_item) }
-
+    let!(:cart_item) { create(:cart_item, cart: cart, product: product, quantity: 1) }
     context 'when the product already is in the cart' do
       subject do
         post '/cart/add_items', params: { product_id: product.id, quantity: 1 }, as: :json
       end
 
       it 'updates the quantity of the existing item in the cart' do
-        expect { subject }.to change { cart_item.reload.quantity }.by(2)
+        expect { subject }.to change { cart_item.reload.quantity }.to(2)
       end
     end
   end
